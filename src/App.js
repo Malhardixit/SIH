129d4993a20ddbf56bfd26ff6700cc749f76c84e
Naman <namansharmatcr@gmail.com>
2022-03-14 22:56:35 +0530

    ➕ Add : Bottom copyright banner ✅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import styled from "styled-components";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
@@ -7,8 +8,22 @@ function App() {
     <div className="App">
       <Navbar />
       <Login />
+
+      <Banner>
+        <span>&#169; Govt. Of India, New Delhi, India</span>
+      </Banner>
     </div>
   );
 }
+
+const Banner = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 10px 0;
+  position: absolute;
+  bottom: 0;
+  background-color: #ff5733;
+  color: #ffffff;
+`;
 
 export default App;
